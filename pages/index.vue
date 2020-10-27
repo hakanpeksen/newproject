182332fdea69661193d45e5733e03626af4e03f0
hakan <32062772+hakanpeksen@users.noreply.github.com>
2020-10-27 12:48:24 +0300

    fontlar  eklendi ve düzenlendi

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,7 @@
         </div>
       </div>
     </div>
+
     <div class="button is-static is-outlined is-success source-icon">
       <span class="icon-news"></span>
     </div>
@@ -32,13 +33,47 @@
     <div class="button is-static is-outlined is-info source-icon">
       <span class="icon-twitter"></span>
     </div>
+    <div class="button is-static is-outlined is-info source-icon">
+      <span class="icon-send"></span>
+    </div>
+    <div class="button is-static is-outlined is-info source-icon">
+      <span class="icon-share"></span>
+    </div>
+    <div class="button is-static is-outlined is-info source-icon">
+      <span class="icon-email"></span>
+    </div>
+    <div class="button is-static is-outlined is-info source-icon">
+      <span class="icon-comment"></span>
+    </div>
+    <span class="yt-icon icon-like"></span>
+    <span class="yt-icon icon-dislike"></span>
+    <div class="source-icon"><span class="icon-plus"></span></div>
+    <div class="source-icon"><span class="icon-minus"></span></div>
+    <div class="source-icon"><span class="icon-check"></span></div>
+    <div class="source-icon"><span class="icon-close"></span></div>
+    <div class="source-icon"><span class="icon-trash"></span></div>
 
     <button class="button is-primary">
       <span class="mdi mdi-plus"></span>NEW
     </button>
     <button class="button is-primary">
+      <span class="mdi mdi-check"></span>
+    </button>
+    <button class="button is-primary">
+      <span class="mdi mdi-close"></span>
+    </button>
+    <button class="button is-primary">
+      <span class="mdi mdi-remove"></span>
+    </button>
+
+    <button class="button is-primary">
       <span class="mdi mdi-eye-off"></span>
     </button>
+    <span class="lt-icon icon-left"></span>
+
+    <span class="lt-icon icon-right"></span>
+    <span class="lt-icon icon-up"></span>
+    <span class="lt-icon icon-down"></span>
   </form>
 </template>
 
