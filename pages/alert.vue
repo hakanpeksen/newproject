8287ae056eca28cc53b9b668cbf3d69472141c77
hakan <32062772+hakanpeksen@users.noreply.github.com>
2020-11-03 18:12:27 +0300

    moment.js  eklendi,  alert.vue sayfasında checkTokenTime  fonksiyonu ile örnek yapıldı.

--- pages/alert.vue
+++ pages/alert.vue
@@ -2,15 +2,48 @@
   <div>
     <mainheader theme="dark" />
     <section class="section">alert sayfası</section>
+    {{ moment(new Date().getTime() / 1000 + 60 * 30) }}
+    {{ Math.floor(new Date().getTime() / 1000) }}
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import mainheader from '~/components/mainheader'
 export default {
   name: 'Alert',
   components: {
     mainheader
+  },
+  data() {
+    return {
+      date: '',
+      moment: moment
+    }
+  },
+  mounted() {
+    this.checkTokenTime()
+  },
+  methods: {
+    checkTokenTime() {
+      var now = new Date().getTime() + 3 * 60 * 1000
+      console.log(now) // 30dk sonrası ms cinsi  //1604413297751
+      let createdDate = new Date(1604416023639)
+      let expirationDate = createdDate
+      let time = new Date().getTime()
+      if (time >= +expirationDate) {
+        console.log('token süresi geçmiş...')
+      } else {
+        let timerSecond = +expirationDate - time
+        this.setTimeoutTimer(timerSecond)
+      }
+    },
+    setTimeoutTimer(expiresIn) {
+      console.log(expiresIn)
+      setTimeout(() => {
+        alert('logout')
+      }, expiresIn)
+    }
   },
   head() {
     return {
